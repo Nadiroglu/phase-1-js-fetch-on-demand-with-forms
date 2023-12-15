a1f34896e2d5685bd741745ef4336c5032cd12d6
nadir0glu <nadiroglunail@gmail.com>
2023-12-15 15:24:22 -0500

    app

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 const init = () => {
+    let form = document.querySelector('form')
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        let input = event.target["searchByID"]   
+        // form.reset()
+        // console.log(input)
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data)
+            let title = document.querySelector('section#movieDetails h4');
+            let summary = document.querySelector('section#movieDetails p');
+            title.innerHTML = data.title
+            summary.innerHTML = data.summary
+            form.reset()
+        })
+         
+    })
+    
   
 }
 
 document.addEventListener('DOMContentLoaded', init);
+
+
